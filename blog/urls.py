3bbe292bec74ae6d22b3fbcc4398717a6a1cc2e1
Melvinxx <jossue_el_86@hotmail.com>
2024-10-18 17:23:23 -0300

    Vista post

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import post_list, post_detail
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('about/', views.about, name='about'),
     path('contacto/', views.contacto, name='contacto'),
-    path('post/new/', views.create_post, name='create-post'),  # URL para crear nuevas publicaciones
+    path('post/new/', views.create_post, name='create-post'),  
+    path('post/<int:post_id>/', post_detail, name='post_detail'),
 ]
